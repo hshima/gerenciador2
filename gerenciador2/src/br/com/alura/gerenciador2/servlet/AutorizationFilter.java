5086acca4e0a15c3e9208fd09f62df67de263c67
hshimada <shimada.henrique@gmail.com>
2020-04-25 11:57:41 -0300

    Adicionados metodos init e destroy para implementação em Jetty

--- gerenciador2/src/br/com/alura/gerenciador2/servlet/AutorizationFilter.java
+++ gerenciador2/src/br/com/alura/gerenciador2/servlet/AutorizationFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -15,7 +16,13 @@ import javax.servlet.http.HttpSession;
 
 @WebFilter("/entry")
 public class AutorizationFilter implements Filter {
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
 
+	@Override
+	public void destroy() {}
+	
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		System.out.println("AutorizationFilter");
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
